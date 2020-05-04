1ddbab547ce2b834f77c262dbadc282fa712d1a1
Ryan Zagrodnik <rzagrodnik@synercomm.com>
2020-05-04 12:15:27 -0500

    edited comments again

--- inforecon.py
+++ inforecon.py
@@ -15,7 +15,7 @@ print("\n"+str(req.headers))
 gethostby_ = socket.gethostbyname(sys.argv[1])
 print("\nThe IP address of "+sys.argv[1]+" is: "+gethostby_ + "\n")
 
-# Get location, region, city, county from ipinfo.io
+#Get location, region, city, county from ipinfo.io
 
 req_two = requests.get("https://ipinfo.io/"+gethostby_+"/json")
 resp_ = json.loads(req_two.text)
