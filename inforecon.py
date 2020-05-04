3474cb86e0ce44e9f5bf3bdabe88c296aa6cb563
Ryan Zagrodnik <rzagrodnik@synercomm.com>
2020-05-04 15:54:16 -0500

    formats

--- inforecon.py
+++ inforecon.py
@@ -7,18 +7,18 @@ if len(sys.argv) < 2:
     print("Usage: "  + sys.argv[0] + " <url>")
     sys.exit(1)
 
-#Get Headers
+#Get Headers and print
 req = requests.get("http://"+sys.argv[1])
 print("\n"+str(req.headers))
 
-#Get IP
+#Get IP and print
 gethostby_ = socket.gethostbyname(sys.argv[1])
 print("\nThe IP address of "+sys.argv[1]+" is: "+gethostby_ + "\n")
 
-#Get location, region, city, county from ipinfo.io
-
+#Get location, region, city, county from ipinfo.io and print
 req_two = requests.get("https://ipinfo.io/"+gethostby_+"/json")
 resp_ = json.loads(req_two.text)
+
 
 print("Location: "+resp_["loc"])
 print("Region: "+resp_["region"])
