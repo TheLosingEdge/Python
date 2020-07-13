8e800ad228ef92987e2f31d67cc4a0f2be0f0948
Ryan Zagrodnik <rzagrodnik@synercomm.com>
2020-07-13 07:17:39 -0500

    class files

---
+++ classes_workmode.py
@@ -0,0 +1,12 @@
+class BasicMath:
+    def summing(self, a, b):
+        return a + b
+    def multy(self, a, b):
+        return a * b
+    def raiseit(self, a, b):
+        return a**b
+
+first_calc = BasicMath()
+first_calc.multy(8,3)
+second_calc = BasicMath()
+second_calc.raiseit(4,5)
